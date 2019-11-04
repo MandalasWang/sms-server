0d96e0c3bb50283d80b959af35cfff11fc827652
有缘 <single_violet@163.com>
2019-11-04 14:19:51 +0800

    2019-11-04 修改controller名称

--- src/main/java/ink/boyuan/smsserver/TestSend.java
+++ src/main/java/ink/boyuan/smsserver/TestSend.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @RequestMapping("/")
-public class Controller {
+public class TestSend {
 
     @Autowired
     private SmsResponse smsResponse;

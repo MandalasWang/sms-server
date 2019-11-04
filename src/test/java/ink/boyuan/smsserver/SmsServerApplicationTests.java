23201979e2610864c7834ecfcd1da24eb73e2ba3
有缘 <single_violet@163.com>
2019-11-04 14:16:53 +0800

    2019-11-04 master commit

---
+++ src/test/java/ink/boyuan/smsserver/SmsServerApplicationTests.java
@@ -0,0 +1,18 @@
+package ink.boyuan.smsserver;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmsServerApplicationTests {
+
+    @Test
+   public void contextLoads() {
+
+    }
+
+}
